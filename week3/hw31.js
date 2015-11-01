be0d42bb9148f6a0061eab1e633a7da7eaa43faf
Will Schutt <will.schutt@live.com>
2015-11-01 01:18:47 -0700

    Finish hw31

--- week3/hw31.js
+++ week3/hw31.js
@@ -8,21 +8,29 @@ MongoClient.connect('mongodb://localhost:27017/school', function (err, db) {
 	var data = db.collection('students');
 	var cursor = data.find({});
 	
-	var lowestAvgScoreBefore = "";
-	var lowestAvgScoreAfter = "";
+	var highAvgScoreAfter = 0;
 	cursor.each(function (err, doc) {
 		if (err) throw err;
 		if (doc == null) {
-			db.close();
+			return;
 		} else {
 			if (doc.scores.length > 0) {
-				console.log('Average score before: ' + averageScores(doc.scores));
-				// data.save(doc, function (err, saved) {
-				// 	if (err) throw err;
-				// 	console.dir("Successfully saved " + saved + " document!");
-				// });
+				//console.log('Average score before: ' + averageScores(doc.scores));
+				console.log("Before  ");
+				console.dir(doc.scores);
+				doc.scores = removeLowestHomeworkScore(doc.scores);
+				console.dir("After ");
+				console.dir(doc.scores);
+				data.save(doc, function (err, saved) {
+					if (err) throw err;
+					console.dir("Successfully saved " + saved + " document!");
+				});
 				//console.dir(doc);
-				console.log('Average score after: ' + averageScores(removeLowestHomeworkScore(doc.scores)));
+				var averageScore = averageScores(doc.scores);
+				if (averageScore > highAvgScoreAfter) {
+					highAvgScoreAfter = averageScore;
+					console.log(doc._id + ' - New high: ' + highAvgScoreAfter);
+				}
 			}
 		}
 	});
@@ -31,7 +39,7 @@ MongoClient.connect('mongodb://localhost:27017/school', function (err, db) {
 var removeLowestHomeworkScore = function(scores) {
 	scores.sort(scoreComparator);
 	for (var i = 0; i < scores.length; i++) {
-		if (scores.type = "homework") {
+		if (scores[i].type == "homework") {
 			scores.splice(i,1);
 			break;
 		}
